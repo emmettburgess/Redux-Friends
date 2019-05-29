e3fb2a18141a0ecb78e9e53900e5d0fd1e903212
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-05-29 17:35:27 -0600

    app updated

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PrivateRoute from './components/privateRoute';
+import Friends from './components/friends';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  return(
+    <div>
+      <header>
+        My Friends
+        <button>
+          <Link to='./login'>Login</Link>
+        </button>
       </header>
     </div>
+
+    <Route path='/login' component={Login} />
+    <PrivateRoute exact path='/friends' component={Friends} />
   );
 }
 
